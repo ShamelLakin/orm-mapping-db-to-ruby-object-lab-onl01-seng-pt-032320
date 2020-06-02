a2b6446273203fce965c40455d5cf83b130eff8e
Shamel Lakin <christlife4u@gmail.com>
2020-06-02 13:44:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,6 +11,8 @@ class Student
   end
 
   def self.all
+    sql <<- SQL 
+    SELECT * FROM student
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
   end
