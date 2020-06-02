c1e35b1c4c5207cda680ce768aff4b7b61b7c2dd
Shamel Lakin <christlife4u@gmail.com>
2020-06-02 14:31:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -93,7 +93,7 @@ class Student
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
       end
-   end	  
+    end	  
     
     def self.first_student_in_grade_10
     sql = <<-SQL
