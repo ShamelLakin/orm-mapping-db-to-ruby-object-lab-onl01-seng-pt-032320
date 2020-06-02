f6119d4f0acdd7f85059e1d3bf21ee3aef68ed85
Shamel Lakin <christlife4u@gmail.com>
2020-06-02 13:45:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
     sql <<- SQL 
     SELECT * FROM Students
     SQL 
-    DB[:conn](sql)
+    DB[:conn].execute(sql)
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
   end
