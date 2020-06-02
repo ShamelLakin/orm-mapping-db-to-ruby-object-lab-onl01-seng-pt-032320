2f23753bd38bcc4b9d1a2f8ddcece0d312c64a14
Shamel Lakin <christlife4u@gmail.com>
2020-06-02 13:58:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,7 +27,7 @@ class Student
       SELECT *
       FROM students
       WHERE name = ?
-      LIMIT 1
+      LIMIT 0
     SQL
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
