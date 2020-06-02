ee0482e69ecdbc2490236b77750a7c69ef512e87
Shamel Lakin <christlife4u@gmail.com>
2020-06-02 14:35:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -91,7 +91,7 @@ class Student
     WHERE students = 10
     LIMIT ?
     SQL
-    DB[:conn].execute(sql).collect do |row|
+    DB[:conn].execute(sql, x).collect do |row|
       self.new_from_db(row)
       end
     end	  
