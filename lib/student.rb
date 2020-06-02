32871802efc7de1382b45abcd91ab0f78daa153d
Shamel Lakin <christlife4u@gmail.com>
2020-06-02 14:33:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -88,7 +88,8 @@ class Student
     sql = <<-SQL
     SELECT * 
     FROM students
-    WHERE students = X
+    WHERE students = 10
+    LIMIT ?
     SQL
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
