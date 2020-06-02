258f9a502587b8ae721fec3882f96cb240ed3c90
Shamel Lakin <christlife4u@gmail.com>
2020-06-02 13:53:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,6 +23,13 @@ class Student
   end
 
   def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
     # find the student in the database given a name
     # return a new instance of the Student class
   end
